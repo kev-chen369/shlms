dc9aec37a80e23fe04e4a0352b0ff0bb458813bd
today1 <cmx@836666>
2026-09-15 19:14:21 +0800

    feat: add runnable API service skeleton

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/kev-chen369/shlms
+
+go 1.26.0
---
+++ internal/httpapi/router.go
@@ -0,0 +1,15 @@
+package httpapi
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+func NewRouter() http.Handler {
+	mux := http.NewServeMux()
+	mux.HandleFunc("GET /healthz", func(w http.ResponseWriter, _ *http.Request) {
+		w.Header().Set("Content-Type", "application/json; charset=utf-8")
+		_ = json.NewEncoder(w).Encode(map[string]string{"status": "ok"})
+	})
+	return mux
+}
